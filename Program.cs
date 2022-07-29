d62c18c40961a724a1a38a4bf2369518b8d6c5c1
MrCryptoFantom <rattlesnakefantom@hotmail.com>
2022-07-29 01:25:08 -0500

    Added an int named "poop"

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LXD16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int poop = 10;
+
+            Console.WriteLine(poop);
+        }
+    }
+}
