acdd3490c2d8a630b288e61702ef0268eac14d6b
MrCryptoFantom <rattlesnakefantom@hotmail.com>
2022-08-05 00:19:01 -0500

    General Performance Updates

--- Generator.cs
+++ Generator.cs
@@ -4,14 +4,14 @@ namespace LXD16
 {
     public class Program
     {
-        
+        //This is not required to generate a number unless you're not calling the Generate() method from another script
         public static void Main(string[] args) 
         {
-            Generate();
+            Generate(false);
         }
 
         public static int num;
-        public static void Generate() 
+        public static void Generate(bool allowNegatives)  //The Generate() method takes in a boolean to determine whether it can generate negative numbers
         {
             Random ran = new Random();
             int l = ran.Next(0,5000);
@@ -23,12 +23,29 @@ namespace LXD16
 
             //Final Product
             int x = d^16;
-            if(x < 1) 
+            if(x < 1 && !allowNegatives) 
             {
                 //If the number is negative, regenerate the number
-                Generate();
+                Generate(false);
             }
             num = x;
+
+            //Just comment this out or delete it along with the PrintOutput() method, unless you want to print the number to the console.
+            PrintOutput(num);
+        }
+
+        public static void PrintOutput(int num) 
+        {
+            //Prints the generated number to the console along with the length
+            int i2 = 0;
+            foreach(char c in num.ToString()) 
+            {
+                i2++;
+            }
+            Console.WriteLine(num + " [" + i2 + "]");
+
+            Console.ReadLine();
+            Generate(false);
         }
     }
 }
