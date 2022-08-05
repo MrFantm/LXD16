55cbf8c222e22d2f36550a242ff483135cceeae5
MrCryptoFantom <rattlesnakefantom@hotmail.com>
2022-08-05 00:18:13 -0500

    Edited the generation itself; Added new bool

--- LXD16.py
+++ LXD16.py
@@ -2,17 +2,32 @@ import random
 
 num = int
 
-def generate():
-    ran = random.Random()
-    l = ran.Next(0,5000)
+def generate(allowNegatives): #The generate() method takes in a boolean to determine whether it can generate negative numbers
+    l = random.randint(0,5000)
     d = 0
 
-    for i in range(0,l):
+    i = 0
+    for j in range(0,l):
         d += i * i^16
-    
+        i += 1
+
     #Final Product
     x = d^16
-    if(x < 1):
+
+    if(x < 1 & allowNegatives == False):
         #If the number is negative, regenerate the number
-        generate()
+        generate(False)
+    
     num = x
+    print(num)
+
+    wait() #Comment this out if you don't plan on interacting with a console
+
+
+
+def wait(): #Comment this out if you don't plan on interacting with a console
+    input()
+    generate(True)
+
+generate(True)
+
